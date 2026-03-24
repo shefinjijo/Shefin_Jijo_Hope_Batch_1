b5f9a95e83a27885b20971b208bfe261d173eedc
shefinjijo <sfshefinjijo@gmail.com>
2026-03-24 09:06:56 +0530

    Delete day 1 directory

--- day 1/day 2/initalization.java
+++
@@ -1,9 +0,0 @@
-public class initalization {
-    public static void main(String[] args) {
-        double a = 10.5;
-        int b;
-        b = (int) a;
-        System.out.println(a);
-        System.out.println(b);
-    }
-}
